5c9f529144ec7af8b282b2c970f285ece00102ee
JeanJeanGSC <jeancouturier.24@gmail.com>
2024-11-15 11:52:00 -0500

    work in progress sur header responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     RouterOutlet,
     SharedModule,
-
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
