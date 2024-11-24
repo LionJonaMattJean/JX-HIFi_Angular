41c680bae50ebd787b757e67ffcdd8c8809c6293
unknown <lionarar@hotmail.com>
2024-11-24 13:00:52 -0500

    creation component findStore

---
+++ src/app/pages/findStore-lion/find-store/find-store.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-store',
+  standalone: true,
+  imports: [],
+  templateUrl: './find-store.component.html',
+  styleUrl: './find-store.component.css'
+})
+export class FindStoreComponent {
+
+}
