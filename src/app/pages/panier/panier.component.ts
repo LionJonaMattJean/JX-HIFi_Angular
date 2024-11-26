fce5092484574c8559b2a4a080c9a6a8de10c6e1
unknown <lionarar@hotmail.com>
2024-11-26 17:33:21 -0500

    added couponCode component to html of Panier

--- src/app/pages/panier/panier.component.ts
+++ src/app/pages/panier/panier.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ShoppingCart } from '../../models/ShoppingCart';
 import { ItemInCartComponent } from "./item-in-cart/item-in-cart.component";
 import { SubTotalComponent } from "./sub-total/sub-total.component";
+import { CouponCodeComponent } from "./coupon-code/coupon-code.component";
 
 @Component({
   selector: 'app-panier',
   standalone: true,
-  imports: [ItemInCartComponent, SubTotalComponent],
+  imports: [ItemInCartComponent, SubTotalComponent, CouponCodeComponent],
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.css'
 })
