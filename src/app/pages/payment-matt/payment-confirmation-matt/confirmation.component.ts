7288661e55f57a85ec0dea8627bbc848d8ca942a
Matthieu Haineault <matthieu_haineault@hotmail.com>
2024-11-26 15:19:51 -0500

    component shopping cart

--- src/app/pages/payment-matt/payment-confirmation-matt/confirmation.component.ts
+++ src/app/pages/payment-matt/payment-confirmation-matt/confirmation.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { OnInit } from '@angular/core';
-import { OrderService } from '../../services/order.service';
+import { OrderService } from '../../../services/order.service';
 
 
 @Component({
---
+++ src/app/pages/tracking-order-matt/tracking-order.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tracking-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './tracking-order.component.html',
+  styleUrl: './tracking-order.component.css'
+})
+export class TrackingOrderComponent {
+
+}
