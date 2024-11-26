6513f0a9b5a005ad21b9ddd8893b12020527e109
unknown <lionarar@hotmail.com>
2024-11-26 15:11:04 -0500

    Presentation de la page Find Store est fini

--- src/app/pages/findStore-lion/find-store/find-store.component.ts
+++ src/app/pages/findStore-lion/find-store/find-store.component.ts
@@ -1,37 +1,33 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '../../../models/Store';
 import { StoresService } from '../../../services/stores.service';
-import { CommonModule } from '@angular/common';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { StoreCarteComponent } from "../store-carte/store-carte.component";
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-find-store',
   standalone: true,
-  imports: [CommonModule, NgFor, StoreCarteComponent, FormsModule],
+  imports: [CommonModule, StoreCarteComponent, FormsModule, NgFor],
   templateUrl: './find-store.component.html',
   styleUrl: './find-store.component.css'
 })
 export class FindStoreComponent implements OnInit {
   storeList: Store[] = [];
-  selectedStore?: Store;
+  storeServ: StoresService = inject(StoresService);
 
-  constructor(private storeServ: StoresService){
+  constructor(){}
 
-  }
-
-  ngOnInit(): void {
+  ngOnInit(): void{
     this.getMagasins();
   }
 
   getMagasins():void{
-    this.storeServ.getAllStores().subscribe(s => this.storeList = s);
-  }
-
-
-  onSelect(magasin: Store):void{
-    this.selectedStore = magasin;
+    this.storeServ.getAllStores().subscribe(s =>{
+      this.storeList = s
+      //console.log(s);
+    } );
+    
   }
 
 }
--- src/app/pages/findStore-lion/store-carte/store-carte.component.ts
+++ src/app/pages/findStore-lion/store-carte/store-carte.component.ts
@@ -1,32 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Store } from '../../../models/Store';
 import { StoresService } from '../../../services/stores.service';
 import { ActivatedRoute } from '@angular/router';
-import { NgIf } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
+import { FormsModule, } from '@angular/forms';
 
 @Component({
   selector: 'app-store-carte',
   standalone: true,
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule,NgIf, NgFor],
   templateUrl: './store-carte.component.html',
   styleUrl: './store-carte.component.css'
 })
 export class StoreCarteComponent {
-  @Input() magasin?:Store;
-
-  constructor(
-    private storeServ: StoresService,
-    private route: ActivatedRoute
-  ){}
-
-  ngOnInit():void{
-    this.getMagasin();
-  }
-
-  getMagasin():void{
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.storeServ.getStoreById('id').subscribe(magasin => this.magasin = magasin);
-  }
+  @Input() magasinInput!:Store;
 
 }
