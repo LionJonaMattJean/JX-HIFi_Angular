e90458fd4fc0147140fefc0151c4e817f45a27ec
unknown <lionarar@hotmail.com>
2024-11-26 16:48:44 -0500

    creer componsant couponcode subtotal et itemincart

---
+++ src/app/pages/panier/coupon-code/coupon-code.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coupon-code',
+  standalone: true,
+  imports: [],
+  templateUrl: './coupon-code.component.html',
+  styleUrl: './coupon-code.component.css'
+})
+export class CouponCodeComponent {
+
+}
