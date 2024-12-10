64ebbdbc6f855f5fb45a17d89460dfa041c29be4
Lion Arar <lionarar@hotmail.com>
2024-12-09 20:26:30 -0500

    resolved

--- src/app/pages/account-details/account-sidebar/account-sidebar.component.ts
+++ src/app/pages/account-details/account-sidebar/account-sidebar.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterLink } from '@angular/router';
-=======
 import {RouterLink, RouterLinkActive} from '@angular/router';
->>>>>>> dbb0bfdf3282912fe28f320fcc171dcef1e623fd
 
 
 
 @Component({
   selector: 'app-account-sidebar',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterLink],
-=======
   imports: [
     RouterLink,
     RouterLinkActive
   ],
->>>>>>> dbb0bfdf3282912fe28f320fcc171dcef1e623fd
   templateUrl: './account-sidebar.component.html',
   styleUrl: './account-sidebar.component.css'
 })
