0ce58564b1ec9978279fdafe08a70d7411b5406d
JeanJeanGSC <jeancouturier.24@gmail.com>
2024-12-12 08:49:50 -0500

    commit before pull

--- src/app/pages/main-site/main-site.component.ts
+++ src/app/pages/main-site/main-site.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import {AcceuilLionComponent} from '../acceuil-lion/acceuil-lion.component';
-import {FooterComponent} from '../../shared/footer-lion/footer.component';
-import {HeaderComponent} from '../../shared/header-jean/header.component';
+import { AcceuilLionComponent } from '../acceuil-lion/acceuil-lion.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-main-site',
   standalone: true,
   imports: [
     AcceuilLionComponent,
-    FooterComponent,
-    HeaderComponent
+    SharedModule,
   ],
   templateUrl: './main-site.component.html',
   styleUrl: './main-site.component.css'
