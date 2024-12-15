2655581a8b8a9bc329d7022b8ad4d400dc8ce206
Matthieu Haineault <matthieu_haineault@hotmail.com>
2024-12-14 19:26:14 -0500

    formTransaction terminé et paufiné

--- src/app/pages/payment-matt/info-card-payment/payment-choices/payment-choices.component.ts
+++ src/app/pages/payment-matt/info-card-payment/payment-choices/payment-choices.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-choices',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './payment-choices.component.html',
   styleUrl: './payment-choices.component.css'
 })
 export class PaymentChoicesComponent {
+
+  @Input() modePaiement: string ='----';
+  @Output() modePaiementChange = new EventEmitter<string>();
+
+  constructor(){}
+
+  onModePaiementChange(){
+    this.modePaiementChange.emit(this.modePaiement);
+  }
 
 }
