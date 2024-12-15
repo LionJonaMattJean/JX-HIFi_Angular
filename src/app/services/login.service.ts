c4f46bd47fcc5d35e40d1db3322caf25d61afdf4
Matthieu Haineault <matthieu_haineault@hotmail.com>
2024-12-15 11:38:13 -0500

    ajustement du service login pour implémenté BehaviorSubject et Observable

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject,Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  //sert à stocké le customerId pour aller le rechercher dans la confirmation de transaction
+  private customerId = new BehaviorSubject<string | null>(null);
+
   constructor() { }
 
-  logIntoAccount(email: string, pass:string){
+  logIntoAccount(email: string, pass:string,customerId: string): void{
+    this.customerId.next(customerId);
+
     //later implement this with DB
-    console.log(email + "\n" + pass);
+
+    console.log(email + "\n" + pass + "\n" + customerId);
+  }
+
+  getCustomerId(): Observable<string | null>{
+    return this.customerId.asObservable();
   }
 
   createAccount(name:string, email:string, pass:string){
