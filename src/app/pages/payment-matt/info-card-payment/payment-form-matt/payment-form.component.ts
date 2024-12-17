5cfc21507d515ce51b374fc0662862c9de01540e
Matthieu Haineault <matthieu_haineault@hotmail.com>
2024-12-17 18:32:44 -0500

    ajustement regex

--- src/app/pages/payment-matt/info-card-payment/payment-form-matt/payment-form.component.ts
+++ src/app/pages/payment-matt/info-card-payment/payment-form-matt/payment-form.component.ts
@@ -23,6 +23,8 @@ export class PaymentFormComponent {
 
   sameAddress: boolean = false;
   modePaiement: string = '----';
+  cardNumber: string = '';
+
   userInfo: any = {
     name: '',
     address: '',
@@ -30,13 +32,34 @@ export class PaymentFormComponent {
     username: '',
     paymentMode: ''
   };
+
+  cardRegex = {
+    visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+    mastercard: /^(?:5[1-5][0-9]{14}|2[2-7][0-9]{14})$/,
+    paypal: /^\S+@\S+\.\S+$/,
+    interact: null
+  }
   
   constructor(private router: Router){}
   
   toggleSameAddress() {
     this.sameAddress = !this.sameAddress;
   }
+
+  validateCardInput(): boolean {
+    if (this.modePaiement === 'paypal') {
+      return this.cardRegex.paypal.test(this.userInfo.email);
+    } else {
+      const regex = this.cardRegex [this.modePaiement];
+      return regex ? regex.test(this.cardNumber) : false;
+    }
+  }
+
   confirmPayment() {
+    if (!this.validateCardInput()) {
+      alert('Informations de paiement invalides. Veuillez vérifier votre saisie.');
+      return;
+    }
     // Collecte des informations du formulaire dans un objet (userInfo)
     this.userInfo.paymentMode = this.modePaiement;
     // Naviguer vers la page de confirmation en passant les données via 'state'
