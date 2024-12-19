112c9147dd7fac83f9c55ce355764955d1e1ce8d
Jonathan <jonathan9170@gmail.com>
2024-12-18 23:46:07 -0500

    Add special price display for on-sale products
    Updated the product list to show a special price alongside the regular price for items on sale. Adjustments include conditional rendering in the template and importing NgIf in the component.

--- src/app/pages/detail-category-jean/detail-category-child-jean/product-list-jean/product-list-jean.component.ts
+++ src/app/pages/detail-category-jean/detail-category-child-jean/product-list-jean/product-list-jean.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Product } from '../../../../models/Product';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-product-list-jean',
   standalone: true,
   imports: [
     RouterModule,
+    NgIf,
   ],
   templateUrl: './product-list-jean.component.html',
   styleUrl: './product-list-jean.component.css'
